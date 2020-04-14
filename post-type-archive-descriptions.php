aeab3ab4b07e5401b03f75cc280a3a74d0fe0c6d
Jeremy Carlson <info@jeremycarlson.com>
2020-04-13 18:08:38 -0600

    Use custom callback for description page if post type's menu location is not the standard
    - This is a first pass, and while it functions, it doesn’t seem like optimal code just yet. If the edit location is non-standard, then we can’t *necessarily* rely on the $post_type GET parameter to be correct. But the $page parameter is set within the plugin to include the post type in the parameter, so we are using that to set up the appropriate post type description settings page.

--- post-type-archive-descriptions.php
+++ post-type-archive-descriptions.php
@@ -136,6 +136,7 @@ function ptad_enable_pages() {
 				// Check if post type has a particular parent menu location
 				$post_type_object = get_post_type_object( $post_type );
 				$show_in_menu     = $post_type_object->show_in_menu;
+				$callback         = ( $show_in_menu && is_bool( $show_in_menu ) ) ? 'standard' : 'custom';
 		    
 			add_submenu_page(
 				ptad_settings_page_parent( $post_type, $show_in_menu ), // $parent slug
@@ -143,7 +144,7 @@ function ptad_enable_pages() {
 				ptad_settings_menu_label( $post_type, 'name' ), // $menu_label
 				ptad_allow_edit_posts(), // $capability
 				$post_type . '-description', // $menu_slug
-				'ptad_settings_page' // $function
+				'ptad_settings_' . $callback // $function
 			);
 
 		} // end if
@@ -270,11 +271,29 @@ function ptad_editor_field( $args ) {
 }
 
 /**
- * Output settings pages
+ * Output settings pages for standard CPT setup
  */
-function ptad_settings_page() {
+function ptad_settings_standard() {
 	$screen = get_current_screen();
 	$post_type = $screen->post_type;
+	
+	ptad_settings_page( $post_type );
+}
+
+/**
+ * Output settings pages for custom CPT setup
+ */
+function ptad_settings_custom() {
+	$page = $_GET['page'];
+	$post_type = str_replace( '-description', '', $page );
+	
+	ptad_settings_page( $post_type );
+}
+
+/**
+ * Output settings pages
+ */
+function ptad_settings_page( $post_type ) {
 	?>
 	<div class="wrap">
 		<h2><?php echo ptad_settings_page_title( $post_type, 'name' ); ?></h2>
