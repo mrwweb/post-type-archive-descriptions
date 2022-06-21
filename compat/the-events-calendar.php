0098bc4aaec12c04f9b669c97a5fc6887f90468e
Mark Root-Wiley <info@mrwweb.com>
2022-06-21 08:37:45 -0700

    Fix the events calendar integration and update readme

--- compat/the-events-calendar.php
+++ compat/the-events-calendar.php
@@ -2,7 +2,7 @@
 /**
  * Set the template which the Archive Description should appear before in v2 Tribe Events templates - OR - disable automatic output of archive description on Events templates with false
  */
-$before_template = apply_filters( 'ptad_tribe_template_before_include', 'events/components/events-bar' );
+$before_template = apply_filters( 'ptad_tribe_template_before_include', 'events/v2/components/events-bar' );
 if( $before_template ) {
 
 	add_action( 'tribe_template_before_include:' . esc_attr( $before_template ), 'ptad_tribe_events_archive_header' );
